f5aaa87dcd862d5a3e1a2d82c1fb8ebcd627b801
ianalejandrook14x <taiwam34@gmail.com>
2024-10-08 02:31:52 -0300

    🍬

---
+++ plugins/game-casino.js
@@ -0,0 +1,42 @@
+  cooldowns[m.sender] = Date.now()
+	    
+        count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+        count = Math.max(1, count)
+        if (args.length < 1) return conn.reply(m.chat, '🛂 Ingresa la cantidad de ' + `*🪙 NanoCoins*` + ' que deseas aportar contra' + ` *Eiai Nano*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+
+        if (user.limit >= count * 1) {
+            user.limit -= count * 1
+            if (Aku > Kamu) {
+                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *The broly-bot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🪙 NanoCoins.`.trim(), m, rcanal)
+            } else if (Aku < Kamu) {
+                user.limit += count * 2
+                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *The broly-bot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🪙 NanoCoins.`.trim(), m, rcanal)
+            } else {
+                user.limit += count * 1
+                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *The broly-bot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🪙 NanoCoins.`.trim(), m, rcanal)
+            }
+        } else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🪙 NanoCoins* para apostar!`.trim(), m, rcanal)
+    
+}
+    
+handler.help = ['apostar *<cantidad>*']
+handler.tags = ['game']
+handler.command = /^(apostar|casino)$/i
+handler.register = true
+
+handler.fail = null
+
+export default handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
+
+function segundosAHMS(segundos) {
+  let segundosRestantes = segundos % 60
+  return `${segundosRestantes} segundos`
+}
+
+function formatNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
---
+++ plugins/game-ruleta.js
@@ -0,0 +1,36 @@
+let cooldowns = {}
+
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+  let users = global.db.data.users[m.sender]
+  
+  let tiempoEspera = 10
+  
+  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
+    let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
+    conn.reply(m.chat, `🚀 Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente`, m, rcanal)
+    return
+  }
+  
+  cooldowns[m.sender] = Date.now()
+
+  if (!text) return conn.reply(m.chat, `🚀 Debes ingresar una cantidad de *🪙 NanoCoins* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m, rcanal)
+
+  let args = text.trim().split(" ")
+  if (args.length !== 2) return conn.reply(m.chat, `🚀 Formato incorrecto. Debes ingresar una cantidad de *🪙 NanoCoins* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m, rcanal)
+
+  let limit = parseInt(args[0])
+  let color = args[1].toLowerCase()
+
+  if (isNaN(limit) || limit <= 0) return conn.reply(m.chat, `🚀 Por favor, ingresa una cantidad válida para la apuesta.`, m, rcanal)
+
+  if (limit > 50) return conn.reply(m.chat, "🚀 La cantidad máxima de apuesta es de 50 *🪙 NanoCoins*.", m, rcanal)
+
+  if (!(color === 'black' || color === 'red')) return conn.reply(m.chat, "🚀 Debes apostar a un color válido: *black* o *red*.", m, rcanal)
+
+  if (limit > users.limit) return conn.reply(m.chat, "🚀 No tienes suficientes *🪙 NanoCoins* para realizar esa apuesta.", m, rcanal)
+  
+  await conn.reply(m.chat, `🚀 Apostaste ${limit} *🪙 NanoCoins* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m, rcanal)
+
+  setTimeout(() => {
+    let result = Math.random()
+    let win = false
